7a84c6931b33c81ff7c86b7a94861d9a642509e0
BxOxSxS <blazejsadowski2004@gmail.com>
2020-06-10 21:39:08 +0200

    Optimization for displaying package information and remove `clear` in script

--- clean.sh
+++ clean.sh
@@ -9,7 +9,6 @@ while IFS= read -r line; do
      pkg[$n]=$line
      n=$((n+1))
      done < $f
-clear
 n3=0
 for ((n2=1;n2<${n};n2++))
 do
@@ -17,13 +16,12 @@ do
      case $yn in
           [Ss]* ) n2=${n};;
           [Cc]* ) exit ;;
-          [Ii]* ) pacman  -Q --info ${pkg[${n2}]} && n2=$((n2-1)) ;;
+          [Ii]* ) echo && echo Info about ${pkg[${n2}]}: && pacman  -Q --info ${pkg[${n2}]} && n2=$((n2-1)) ;;
           [Yy]* ) echo ${pkg[${n2}]} >> $fc ;n3=$((n3+1));;
           [Nn]* ) ;;
           * ) echo "Please answer yes or no"; n2=$((n2-1)) ;;
      esac
 done
-clear
 echo "Number of selected packages: $n3/$n"
 sudo pacman -R - < $fc
 rm $f
